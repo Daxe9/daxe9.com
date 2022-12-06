a6a0293ae339159695c520b07835ab08551e2463
_xie <davidexie694@gmail.com>
2022-12-06 22:23:37 +0100

    added a spacer component

---
+++ src/components/Spacer.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+	space: {
+		type: Number,
+		default: 1,
+	}
+});
+
+let spacer = "";
+for (let i = 0; i < props.space; i++) {
+	spacer += "&nbsp;";
+}
+</script>
+
+<template>
+	<span v-html="spacer"></span>
+</template>
+
